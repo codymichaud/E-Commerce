92a108bb97e4c72e1632cceca3811731ce4b9f16
Cody Michaud <72232974+codymichaud@users.noreply.github.com>
2021-03-31 17:53:57 -0700

    Deleting product.post route. Debugging post error

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -48,32 +48,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create({
-    product_name: req.body.product_name,
-    price: req.body.price,
-    stock: req.body.stock,
-    tagIds: req.body.tagIds,
-    category_id: req.body.category_id
-  })
 
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    }).then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((error) => {
-      console.log(error);
-      res.status(400).json(error);
-    });
 });
 
 // update product
